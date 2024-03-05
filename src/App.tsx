c33b071c09a93892c1d14beb8699f518d53341fa
arefa313 <arefa.noori.1380@gmail.com>
2024-03-05 04:49:46 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+let name :string;
+let age : number;
+let isStudent :boolean;
+let hobbies : string[];
+let role :[number , string];
+
+// type Person ={
+//   name: string;
+//   age? :number;
+// };
+// let person : Person={
+//   name:'piyush',
+// };
+// let lotsOfPeople :Person[];
+
+function App() {
+  return (
+    <div className="App">
+    hellow word
+    </div>
+  );
+}
+
+export default App;
